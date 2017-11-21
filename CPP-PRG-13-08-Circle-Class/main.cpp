d857dbeb560c699ee7c9c116af1cab5e78e9ab1d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-21 13:31:48 -0500

    Prompt written.

--- CPP-PRG-13-08-Circle-Class/main.cpp
+++ CPP-PRG-13-08-Circle-Class/main.cpp
@@ -5,11 +5,31 @@
 //  Created by Keith Smith on 11/21/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a Circle class that has the following member variables:
+//
+//  * radius - a double
+//  * pi - a double initialized with the value 3.14159
+//
+//  The class should also have the following member functions:
+//
+//  * Default Constructor - a default constructor that sets radius to 0.0
+//  * Constructor - accepts the radius of the circle as an argument
+//  * setRadius - a mutator function for the radius variable
+//  * getRadius - an accessor function for the radius variable
+//  * getArea - returns the area of the circle, which is calculated as
+//      area = pi * radius * radius
+//  * getDiameter - returns the diameter of the circle, which is calculated as
+//      diameter = radius * 2
+//  * getCircumference - returns the circumference of the circle, which is calculated as
+//      circumference = 2 * pi * radius
+//
+//  Write a program that demonstrates the Circle class by asking the user for the
+//  circle's radius, creating a Circle object, then reporting the circle's area, diameter, and
+//  circumference.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
     return 0;
 }
